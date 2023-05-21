b36d3d3739f0687cffe0c24d92cb1dac1b269f77
yencs21 <yencs21@gmail.com>
2023-05-21 00:28:07 -0400

    Hortografia

--- aicros/settings.py
+++ aicros/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Static/'
 STATIC_ROOT = BASE_DIR /'aicrosWeb/Static/'
 MEDIA_URL   = 'aiMedia/'
 MEDIA_ROOT  = BASE_DIR / 'aiMedia'
